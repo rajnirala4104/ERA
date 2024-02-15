9287543dd4e47226746540881a9a11b38f908bb5
rajnirala4194 <rajnirala4104@gmail.com>
2024-02-15 11:22:43 +0530

    built database connection and middelwares

--- BACKEND/api/app.js
+++ BACKEND/api/app.js
@@ -3,6 +3,8 @@ const app = express()
 const asyncHandler = require('express-async-handler')
 const { StatusCodes } = require('http-status-codes')
 const { v1Router } = require('./routes/v1Routes')
+const connectDatabase = require('./config/connectDB')
+connectDatabase()
 
 app.get('/health', asyncHandler(async (req, res) => res.status(StatusCodes.OK)))
 app.use('/api/v1', v1Router)
---
+++ BACKEND/api/config/connectDB.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`mongo has connected - ${connection.connection.host}`);
+    } catch (e) {
+        console.log(`Oops!! something went wrong,  - ${e.message}`);
+        process.exit();
+    }
+};
+
+module.exports = connectDatabase;

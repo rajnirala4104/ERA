84794a0d2c210261e0a4da3669455322f9089cea
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-02-27 14:12:55 +0530

    signup page UI is done

--- FRONTED/ERA/src/components/Singup.tsx
+++ FRONTED/ERA/src/components/Singup.tsx
@@ -1,21 +1,32 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { LoaderSpinner } from '.'
 
 export const Singup = () => {
+    const [hidePassword, setHidePassword] = useState<boolean>(true)
     return (
         <React.Fragment>
             <Suspense fallback={<LoaderSpinner />}>
-                <div>
+                <div className='h-[100%] flex justify-center items-center flex-col'>
                     <div className="inputs">
-                        <div className="inputName">
-                            <input type="text" placeholder='enter your name' />
+                        <div className="inputname my-3 border px-2 py-2 border-[#115f4c] rounded-md flex justify-start items-center">
+                            <input type="text" className='outline-none text-xl' placeholder='Enter your name...' />
                         </div>
-                        <div className="inputEmail">
-                            <input type="text" placeholder='enter your email' />
+                        <div className="inputemail my-3 border px-2 py-2 border-[#115f4c] rounded-md flex justify-start items-center">
+                            <input type="email" className='outline-none text-xl' placeholder='Enter your email...' />
                         </div>
-                        <div className="inputPassword">
-                            <input type="text" placeholder='enter your password' />
+                        <div className="inputPassword mt-3 border px-2 py-1 border-[#115f4c] rounded-md flex justify-between items-center">
+                            <input type={hidePassword ? 'password' : ""} className='outline-none text-xl ' placeholder='Password..' />
+                            <button onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</button>
                         </div>
+                        <div className="inputPassword mt-3 border px-2 py-1 border-[#115f4c] rounded-md flex justify-between items-center">
+                            <input type={hidePassword ? 'password' : ""} className='outline-none text-xl ' placeholder='Confirm Password..' />
+                            <button onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</button>
+                        </div>
+                    </div>
+                    <div className="btn my-4 ">
+                        <button className='text-[20px] text-center cursor-pointer mx-2 w-[100%] bg-black hover:bg-gray-800 dark:text-white px-2 py-2 rounded-md' >
+                            Signup
+                        </button>
                     </div>
                 </div>
             </Suspense>
--- FRONTED/ERA/src/pages/SignupAndLogin.tsx
+++ FRONTED/ERA/src/pages/SignupAndLogin.tsx
@@ -14,17 +14,17 @@ export const SignupAndLogin = () => {
           style={{ background: `url(${allImages.loginBg}) center center/cover` }}
           className='w-full h-screen flex justify-center items-center '>
           <div className='  backdrop-blur-md rounded-lg shadow-[0_00px_60px_-6px_rgba(0,0,0,0.5)] w-[80%] h-[80%] flex justify-between items-center'>
-            <div className="sideImg  flex flex-col justify-between items-center w-[45%] h-[100%]">
+            <div className="sideImg  flex flex-col justify-between items-center w-[45%] h-[100%] ">
               <h1 style={{
                 fontFamily: ` "Fjalla One", sans-serif`,
                 fontWeight: 400,
                 fontStyle: "normal",
-              }} className='text-6xl mt-20 text-[#0c644e] cursor-default'>ERA</h1>
+              }} className='text-5xl mt-20  text-[#0c644e] cursor-default'>ERA</h1>
               <img src={allImages.loginSVG} className='w-[70%] mb-4' alt="era" />
             </div>
-            <div className="w-[50%] b h-[90%] flex justify-center items-center flex-col">
+            <div className="w-[50%] h-[87%] flex justify-center items-center flex-col">
               {loginDesign ? <Login /> : <Singup />}
-              <span onClick={() => setLoginDesign(!loginDesign)} className='text-blue-500 hover:underline cursor-pointer'>Don't have account</span>
+              <span onClick={() => setLoginDesign(!loginDesign)} className='text-blue-500 hover:underline cursor-pointer'>{loginDesign ? "Don't have account" : "have account"}</span>
             </div>
           </div>
         </section>

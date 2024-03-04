ccafc62d8d2d28c6aa0638d76c939b84ee21b164
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-03-04 15:04:41 +0530

    NEW method for FORMS

--- FRONTED/ERA/src/components/Login.tsx
+++ FRONTED/ERA/src/components/Login.tsx
@@ -4,45 +4,44 @@ import { login } from '../api/services/authenticationApiServices'
 
 export const Login = () => {
   const [hidePassword, setHidePassword] = useState<boolean>(true)
-  const [userEmail, setUserEmail] = useState<string>()
-  const [userPassword, setUserPassword] = useState<string>()
 
-  const submitHandler = async () => {
-    if (!userEmail || !userPassword) {
-      alert("invailid email and password")
-    }
+  const formSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.target as any);
+    const formObject = Object.fromEntries(formData.entries())
+
+    if (!formObject.email || !formObject.password) alert("invailid email and password")
+
     try {
-      const { data } = await login({ email: userEmail, password: userPassword })
+      const { data } = await login({ email: formObject.email as string, password: formObject.password as string })
       localStorage.setItem('userInfo', JSON.stringify(data))
-      console.log(data)
       window.location.reload()
-    } catch (error) {
-      alert("something went wrong in submitHandler")
-    }
+    } catch (error) { alert("Oops!! there is an error in code") }
+
   }
 
   return (
     <React.Fragment>
       <Suspense fallback={<LoaderSpinner />}>
-        <div className='h-[100%] flex justify-center items-center flex-col'>
+        <form onSubmit={(e) => formSubmitHandler(e)} className='h-[100%] flex justify-center items-center flex-col'>
           <div>
             <div className="inputemail my-3 border px-2 py-2 border-[#115f4c] rounded-md flex justify-start items-center">
-              <input onChange={(e) => setUserEmail(e.target.value)} type="email" className='outline-none text-xl' placeholder='Enter your email...' />
+              <input name='email' type="email" className='outline-none text-xl' placeholder='Enter your email...' />
             </div>
             <div className=''>
               <div className="inputPassword mt-3 border px-2 py-1 border-[#115f4c] rounded-md flex justify-between items-center">
-                <input onChange={(e) => setUserPassword(e.target.value)} type={hidePassword ? 'password' : ""} className='outline-none text-xl ' placeholder='Password..' />
-                <button onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</button>
+                <input name="password" type={hidePassword ? 'password' : ""} className='outline-none text-xl ' placeholder='Password..' />
+                <span onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</span>
               </div>
               <span className='text-blue-500 hover:text-blue-600 cursor-pointer hover:underline'>Forgot password</span>
             </div>
           </div>
           <div className="btn my-4 ">
-            <button onClick={() => submitHandler()} className='text-[20px] text-center cursor-pointer mx-2 w-[100%] bg-black hover:bg-gray-800 dark:text-white px-2 py-2 rounded-md' >
+            <button type='submit' className='text-[20px] text-center cursor-pointer mx-2 w-[100%] bg-black hover:bg-gray-800 dark:text-white px-2 py-2 rounded-md' >
               Login
             </button>
           </div>
-        </div >
+        </form >
       </Suspense >
     </React.Fragment >
   )
--- FRONTED/ERA/src/components/Singup.tsx
+++ FRONTED/ERA/src/components/Singup.tsx
@@ -4,65 +4,67 @@ import { signup } from '../api/services/authenticationApiServices'
 
 export const Singup = () => {
     const [hidePassword, setHidePassword] = useState<boolean>(true)
-    const [userName, setUserName] = useState<string>()
-    const [userEmail, setUserEmail] = useState<string>()
-    const [userPassword, setUserPassword] = useState<string>()
-    const [confirmPassword, setConfirmPassword] = useState<string>()
+    const [hideConfirmPassword, setHideConfirmPassword] = useState<boolean>(true)
 
-    const subimtHandler = async () => {
-        if (userPassword !== confirmPassword) {
+    const subimtHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData = new FormData(e.target as any);
+        const formObject = Object.fromEntries(formData.entries())
+
+        if (formObject.password !== formObject.confirmPassword) {
             alert("passwords are not same")
+            return;
         }
-        if (!userEmail || !confirmPassword) {
+        if (!formObject.email || !formObject.password) {
             alert("inVailid data given")
+            return;
         }
         try {
             console.log("entered in try")
-            const response = await signup({ email: userEmail, name: userName, password: confirmPassword })
-            console.log(response)
+            const { data } = await signup({ email: formObject.email as string, name: formObject.name as string, password: formObject.confirmPassword as string })
+            console.log(data)
             // localStorage.setItem('userInfo', JSON.stringify(data))
             // window.location.reload()
-        } catch (error) {
-            alert("we can't create your account right now")
-        }
+        } catch (error) { alert("we can't create your account right now") }
     }
 
     return (
         <React.Fragment>
             <Suspense fallback={<LoaderSpinner />}>
-                <div className='h-[100%] flex justify-center items-center flex-col'>
+                <form onSubmit={(e) => subimtHandler(e)} className='h-[100%] flex justify-center items-center flex-col'>
                     <div className="inputs">
                         <div className="inputname my-3 border px-2 py-2 border-[#115f4c] rounded-md flex justify-start items-center">
-                            <input type="text"
-                                onChange={(e) => setUserName(e.target.value)}
+                            <input
+                                type="text"
+                                name="name"
                                 className='outline-none text-xl' placeholder='Enter your name...' />
                         </div>
                         <div className="inputemail my-3 border px-2 py-2 border-[#115f4c] rounded-md flex justify-start items-center">
                             <input type="email"
-                                onChange={(e) => setUserEmail(e.target.value)}
+                                name='email'
                                 className='outline-none text-xl' placeholder='Enter your email...' />
                         </div>
                         <div className="inputPassword mt-3 border px-2 py-1 border-[#115f4c] rounded-md flex justify-between items-center">
                             <input
-                                onChange={(e) => setUserPassword(e.target.value)}
+                                name="password"
                                 type={hidePassword ? 'password' : ""} className='outline-none text-xl ' placeholder='Password..' />
-                            <button onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</button>
+                            <span onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</span>
                         </div>
                         <div className="inputPassword mt-3 border px-2 py-1 border-[#115f4c] rounded-md flex justify-between items-center">
                             <input
-                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                name="confirmPassword"
                                 type={hidePassword ? 'password' : ""} className='outline-none text-xl ' placeholder='Confirm Password..' />
-                            <button onClick={() => setHidePassword(!hidePassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</button>
+                            <span onClick={() => setHideConfirmPassword(!hideConfirmPassword)} className='w-16 text-center cursor-pointer  bg-black dark:text-white px-2 py-2 text-[14px] rounded-md hover:bg-gray-800'>{hidePassword ? "Show" : "Hide"}</span>
                         </div>
                     </div>
                     <div className="btn my-4 ">
                         <button
-                            onClick={() => subimtHandler()}
+                            type='submit'
                             className='text-[20px] text-center cursor-pointer mx-2 w-[100%] bg-black hover:bg-gray-800 dark:text-white px-2 py-2 rounded-md' >
                             Signup
                         </button>
                     </div>
-                </div>
+                </form>
             </Suspense>
         </React.Fragment>
     )

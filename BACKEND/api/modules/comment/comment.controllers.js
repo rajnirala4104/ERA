e9fb1a1030da8ea2f19e3682770057893740b98a
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-03-15 11:04:29 +0530

    built createComment controller

--- BACKEND/api/modules/comment/comment.controllers.js
+++ BACKEND/api/modules/comment/comment.controllers.js
@@ -1,15 +1,51 @@
 import expressAsyncHandler from "express-async-handler";
+import { StatusCodes } from "http-status-codes";
+import { Post } from "../post/post.model";
 
 export const commentControllers = {
-    getAllTheComments: expressAsyncHandler(async (req, res) => {
+    getAllTheComments: expressAsyncHandler(async (req, res) => { }),
 
-    }),
-    getAllTheCommentsOfSinglePost: expressAsyncHandler(async (req, res) => {
+    getAllTheCommentsOfSinglePost: expressAsyncHandler(async (req, res) => { }),
 
-    }),
     createComment: expressAsyncHandler(async (req, res) => {
+        try {
 
+            const { post, content } = req.body;
+            const userId = req.user._id;
+
+            const postExist = await Post.find({ _id: post })
+            if (!postExist) {
+                return res.status(StatusCodes.BAD_REQUEST).json({
+                    message: "post doesn't exist",
+                    status: StatusCodes.BAD_REQUEST,
+                    data: null
+
+                })
+            }
+
+            const newCommentObject = {
+                post: post,
+                user: userId,
+                content: content
+            }
+
+            await Comment.create(newCommentObject)
+            return res.status(StatusCodes.OK).json({
+                message: "comment created successfully",
+                status: StatusCodes.OK,
+                data: newCommentObject
+            })
+
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                message: "we can't create comment for now",
+                status: StatusCodes.INTERNAL_SERVER_ERROR,
+                error: error,
+                data: null
+            })
+        }
     }),
+
     deleteComment: expressAsyncHandler(async (req, res) => {
 
     })

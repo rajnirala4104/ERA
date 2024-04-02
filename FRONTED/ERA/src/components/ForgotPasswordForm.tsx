bc76647f8989dc5f70d55dc2c7cc001c2fb4a649
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-04-02 14:20:14 +0530

    feat(Component): built ForgotPassword component 👍

--- FRONTED/ERA/src/components/ForgotPasswordForm.tsx
+++ FRONTED/ERA/src/components/ForgotPasswordForm.tsx
@@ -1,13 +1,35 @@
-import React, { Fragment, Suspense } from "react";
+import React, { Fragment, Suspense, useState } from "react";
 import { LoaderSpinner } from ".";
+import { Link } from "react-router-dom";
 
 export const ForgotPasswordForm: React.FC = () => {
-   const submitFormHandler = () => {};
+
+   const [newPasswordHideOrShow, setNewPasswordHideOrShow] = useState<boolean>(false)
+
+   const submitFormHandler = (e: React.FormEvent<HTMLFormElement>) => { };
 
    return (
       <Fragment>
          <Suspense fallback={<LoaderSpinner />}>
-            <form onSubmit={() => submitFormHandler()}></form>
+            <div className="border flex flex-col justify-center items-center w-[90%] h-[90%] ">
+               <form
+                  onSubmit={(e) => submitFormHandler(e)}
+                  className=" flex flex-col justify-center items-center  mx-2"
+               >
+                  <div className="email ">
+                     <input type="text" placeholder="Email.." className="px-2 py-2 w-[16.5rem] outline-none border-2 rounded-md border-black hover:border-[#075e48] focus:border-[#075e48] my-2" />
+                  </div>
+                  <div className="newPassword">
+                     <input className="px-2 py-2 w-[16.5rem] outline-none border-2 rounded-md border-black hover:border-[#075e48] focus:border-[#075e48] my-1" type={newPasswordHideOrShow ? "text" : "password"} placeholder="New Passwrod.." />
+                  </div>
+                  <div className="btn my-3">
+                     <button className="bg-[#1aeab6] p-2 rounded-md" type="submit">Create New Password</button>
+                  </div>
+               </form>
+               <div className="">
+                  <Link to='/account' className="underline text-blue-500 ">login</Link>
+               </div>
+            </div>
          </Suspense>
       </Fragment>
    );

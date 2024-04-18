a543117044f5f3e8aee5916460505606fd8fb5e0
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-04-18 17:38:46 +0530

    built controller for getting thought post

--- BACKEND/api/modules/toughtPost/thoughtPost.controller.js
+++ BACKEND/api/modules/toughtPost/thoughtPost.controller.js
@@ -2,9 +2,34 @@ const expressAsyncHandler = require("express-async-handler");
 const { StatusCodes } = require("http-status-codes");
 const { ThoughtPost } = require("./thoughtPost.model");
 const thoughPostControllers = {
-    getAllTherThouhtPosts: expressAsyncHandler(async (req, res) => { }),
+    getAllTherThouhtPosts: expressAsyncHandler(async (req, res) => {
+        try {
+            const data = ThoughtPost.find({});
+            return res.status(StatusCodes.OK).json({
+                message: "here all the thought post",
+                status: StatusCodes.OK,
+                data: data
+            })
+        } catch (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+            throw new Error(error.message)
+        }
+    }),
 
-    getSingleThoughtPostData: expressAsyncHandler(async (req, res) => { }),
+    getSingleThoughtPostData: expressAsyncHandler(async (req, res) => {
+        try {
+            const { thoughtPostId } = req.params;
+            const data = ThoughtPost.find({ _id: thoughtPostId });
+            return res.status(StatusCodes.OK).json({
+                message: "here your thought post",
+                status: StatusCodes.OK,
+                data: data
+            })
+        } catch (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+            throw new Error(error.message);
+        }
+    }),
 
     createThoughtPost: expressAsyncHandler(async (req, res) => {
         try {

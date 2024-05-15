91c4323bb5f6e9db9a7dcf941f3c59b7551adf5a
rajnirala4104 <rajnirala4104@gmail.com>
2024-05-15 13:13:04 +0530

    lil bit changes

--- BACKEND/api/app.js
+++ BACKEND/api/app.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler')
-const { StatusCodes } = require('http-status-codes')
-const { v1Router } = require('./routes/v1Routes')
-const connectDatabase = require('./config/connectDB')
-const { erroHandler, notFoundErr } = require('./middelware/error')
-const cors = require('cors')
-connectDatabase()
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { StatusCodes } = require("http-status-codes");
+const { v1Router } = require("./routes/v1Routes");
+const connectDatabase = require("./config/connectDB");
+const { erroHandler, notFoundErr } = require("./middelware/error");
+const cors = require("cors");
+connectDatabase();
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/health', asyncHandler(async (req, res) => res.status(StatusCodes.OK)))
-app.use('/api/v1', v1Router)
+app.get(
+   "/health",
+   asyncHandler(async (req, res) => res.status(StatusCodes.OK))
+);
+app.use("/api/v1", v1Router);
 
-app.use(erroHandler)
-app.use(notFoundErr)
+app.use(erroHandler);
+app.use(notFoundErr);
 
-
-module.exports = { app }
+module.exports = { app };
--- FRONTED/ERA/src/components/PostCard.tsx
+++ FRONTED/ERA/src/components/PostCard.tsx
@@ -1,57 +1,78 @@
-import React, { Fragment, memo, useContext, useEffect, useState } from 'react'
-import { postInterface, user } from '../interfaces'
-import { PostIcons } from '.'
-import { getDateFromMongoData } from '../utils'
-import { useNavigate } from 'react-router-dom'
-import { EditIcon } from '../icons'
-import { EditPostPopupContext } from '../contaxt'
-import { EditPostPopup } from '../components'
+import React, { Fragment, memo, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { PostIcons } from ".";
+import { EditPostPopupContext } from "../contaxt";
+import { EditIcon } from "../icons";
+import { postInterface, user } from "../interfaces";
+import { getDateFromMongoData } from "../utils";
 
 const PostCard: React.FC<postInterface> = memo((props) => {
+   const [user, setUser] = useState<user>();
+   const navigator = useNavigate();
 
-    const [user, setUser] = useState<user>()
-    const navigator = useNavigate()
+   const { editPostPopupOnOff, setEditPostPopupOnOff } =
+      useContext(EditPostPopupContext);
 
-    const { editPostPopupOnOff, setEditPostPopupOnOff } = useContext(EditPostPopupContext)
+   useEffect(() => {
+      const user = JSON.parse(localStorage.getItem("userInfo") as string);
+      setUser(user);
+   }, []);
 
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("userInfo") as string);
-        setUser(user)
-    }, []);
-
-    return (
-        <Fragment>
-
-            <div className="postCard mx-3 my-6  flex flex-col justify-center items-center rounded-lg shadow-lg bg-white">
-                <div className="userInfo w-[95%] my-2 flex justify-between items-center ">
-                    <div onClick={() => navigator(`/user-profile/${props.user?._id}`)} className='flex cursor-pointer justify-center items-center'>
-                        <img className='w-[2rem] h-[2rem] rounded-full' src={props.user?.profilePic} alt="ERA" />
-                        <span className='mx-2 font-bold'>{props.user?.name}</span>
-                    </div>
-                    <div className='flex'>
-                        <span>{getDateFromMongoData(props.createdAt as string)}</span>
-                        {user?._id === props.user?._id ? (
-                            <span onClick={() => setEditPostPopupOnOff(!editPostPopupOnOff)} className='hover:text-black text-gray-700 mx-2 cursor-pointer'>{<EditIcon classess='text-xl ' />}</span>
-                        ) : ""}
-                    </div>
-                </div>
-                <div className="content w-[95%]">
-                    {/* ------------------------- thought Post Idia ------------ */}
-                    {props.caption ? (
-                        <img className='w-full rounded-md' src={props.content} alt={"ERA post"} />
-                    ) : (
-                        <span className='font-mono font-semibold'>"{props.thought}"</span>
-                    )}
-                </div>
-                <div className="caption w-[90%] mt-2 font-semibold ">
-                    <p>{props.caption}</p>
-                </div>
-                <div className='w-[95%]'>
-                    <PostIcons />
-                </div>
+   return (
+      <Fragment>
+         <div className="postCard mx-3 my-6  flex flex-col justify-center items-center rounded-lg shadow-lg bg-white">
+            <div className="userInfo w-[95%] my-2 flex justify-between items-center ">
+               <div
+                  onClick={() => navigator(`/user-profile/${props.user?._id}`)}
+                  className="flex cursor-pointer justify-center items-center"
+               >
+                  <img
+                     className="w-[2rem] h-[2rem] rounded-full"
+                     src={props.user?.profilePic}
+                     alt="ERA"
+                  />
+                  <span className="mx-2 font-bold">{props.user?.name}</span>
+               </div>
+               <div className="flex">
+                  <span>{getDateFromMongoData(props.createdAt as string)}</span>
+                  {user?._id === props.user?._id ? (
+                     <span
+                        onClick={() =>
+                           setEditPostPopupOnOff(!editPostPopupOnOff)
+                        }
+                        className="hover:text-black text-gray-700 mx-2 cursor-pointer"
+                     >
+                        {<EditIcon classess="text-xl " />}
+                     </span>
+                  ) : (
+                     ""
+                  )}
+               </div>
             </div>
-        </Fragment>
-    )
-})
+            <div className="content w-[95%]">
+               {/* ------------------------- thought Post Idia ------------ */}
+               {props.caption ? (
+                  <div
+                     style={{
+                        background: `url('${props.content}') center center/cover`,
+                     }}
+                     className="w-[100%] h-[33rem]"
+                  ></div>
+               ) : (
+                  <span className="font-mono font-semibold">
+                     "{props.thought}"
+                  </span>
+               )}
+            </div>
+            <div className="caption w-[90%] mt-2 font-semibold ">
+               <p>{props.caption}</p>
+            </div>
+            <div className="w-[95%]">
+               <PostIcons />
+            </div>
+         </div>
+      </Fragment>
+   );
+});
 
-export default PostCard
+export default PostCard;

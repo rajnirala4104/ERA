218c22f467b315301e33732da9ae335456321b88
rajnirala4104 <rajnirala4104@gmail.com>
2024-05-17 14:12:51 +0530

    singlePostPopup is almost done

--- FRONTED/ERA/src/components/PostIcons.tsx
+++ FRONTED/ERA/src/components/PostIcons.tsx
@@ -1,22 +1,22 @@
-import React, { Fragment } from 'react'
-import { CommentIcon, LikeIcon, UnLikeIcon } from '../icons'
+import React, { Fragment } from "react";
+import { CommentIcon, UnLikeIcon } from "../icons";
 
 const PostIcons: React.FC = () => {
-    return (
-        <Fragment>
-            <div className="iconsContainer flex w-[50%] my-4 justify-between ">
-                <div className='flex justify-center items-center hover:bg-green-200 rounded-md cursor-pointer px-2 py-1'>
-                    <UnLikeIcon classess='mx-1 text-2xl' />
-                    <span>Like</span>
-                </div>
-                <span className='text-gray-600'>|</span>
-                <div className='flex justify-center items-center  hover:bg-green-200  rounded-md cursor-pointer  px-2 py-1'>
-                    <CommentIcon classess='mx-1 text-2xl' />
-                    <span>Comment</span>
-                </div>
+   return (
+      <Fragment>
+         <div className="iconsContainer flex w-[50%] my-4 justify-between ">
+            <div className="flex justify-center items-center hover:bg-green-200 rounded-md cursor-pointer px-2 py-1">
+               <UnLikeIcon classess="mx-1 text-2xl" />
+               <span>Like</span>
             </div>
-        </Fragment>
-    )
-}
+            <span className="text-gray-600">|</span>
+            <div className="flex justify-center items-center  hover:bg-green-200  rounded-md cursor-pointer  px-2 py-1">
+               <CommentIcon classess="mx-1 text-2xl" />
+               <span>Comment</span>
+            </div>
+         </div>
+      </Fragment>
+   );
+};
 
 export default PostIcons;
--- FRONTED/ERA/src/components/SinglePostPopup.tsx
+++ FRONTED/ERA/src/components/SinglePostPopup.tsx
@@ -1,5 +1,7 @@
 import React, { Fragment, Suspense, useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { PostIcons } from ".";
 import { SinglePostPopupContext } from "../contaxt";
 import { CloseIcon } from "../icons";
 import { resetState } from "../redux/states/postSlice";
@@ -11,6 +13,8 @@ const SinglePostPopup: React.FC = () => {
       SinglePostPopupContext
    );
 
+   const navigator = useNavigate();
+
    const dispatch = useDispatch();
    const { post } = useSelector((state: RootState) => state);
 
@@ -21,7 +25,7 @@ const SinglePostPopup: React.FC = () => {
                style={{ background: "rgba(0,0,0, 0.1)" }}
                className="w-full h-screen flex justify-center items-center backdrop-blur-md absolute top-0 left-0  bg-black z-10"
             >
-               <div className="centerContainer flex w-[60%] h-[80%] justify-between relative items-center bg-white rounded-md">
+               <div className="centerContainer flex w-[70%] h-[90%] justify-between relative items-center bg-white rounded-lg ">
                   <span
                      onClick={() => {
                         dispatch(resetState());
@@ -37,7 +41,73 @@ const SinglePostPopup: React.FC = () => {
                      </Fragment>
                   ) : (
                      <Fragment>
-                        <div className="post">{post.caption}</div>
+                        <div className="post flex w-full h-full">
+                           <div
+                              className="image  w-[50%]  h-full rounded-tl-md rounded-bl-md"
+                              style={{
+                                 background: `url(${post.content}) center center/cover`,
+                              }}
+                           ></div>
+                           <div className="info w-[50%]">
+                              <div className="upperHeader bg-slate-200 py-3 px-2 flex flex-col justify-center items-start rounded-tr-md">
+                                 <div
+                                    onClick={() =>
+                                       navigator(
+                                          `/user-profile/${post.user?._id}`
+                                       )
+                                    }
+                                    className="flex justify-center items-center cursor-pointer"
+                                 >
+                                    <img
+                                       src={post.user?.profilePic}
+                                       alt="ERA"
+                                       loading="lazy"
+                                       className="w-[3rem] h-[3rem] rounded-full object-cover"
+                                    />
+                                    <p className="mx-2 text-xl">
+                                       {post.user?.name}
+                                    </p>
+                                 </div>
+                              </div>
+                              <div className="caption my-3 px-6 bg-white w-full">
+                                 <p className="text-[13px] font-mono font-semibold">
+                                    "{post.caption}"
+                                 </p>
+                              </div>
+                              <hr />
+                              <div className="comments container overflow-y-auto h-[70%]">
+                                 {/* -------- commentes ------- */}
+                                 <div className="singleComment shadow-md w-[70%] m-2 rounded-md">
+                                    <div className=" cursor-pointer user flex justify-start  p-1 items-center bg-slate-200">
+                                       <img
+                                          src={post.user?.profilePic}
+                                          alt="ERA"
+                                          loading="lazy"
+                                          className="w-[1.5rem] h-[1.5rem] rounded-full object-cover"
+                                       />
+                                       <p className="mx-2 text-[12px]">
+                                          {post.user?.name}
+                                       </p>
+                                    </div>
+                                    <div className="commentContent">
+                                       <p className="text-[13px] p-2">
+                                          Lorem ipsum dolor sit amet
+                                          consectetur, adipisicing elit. Iste
+                                          voluptates a omnis obcaecati,
+                                          distinctio quia quisquam et architecto
+                                          consectetur. Laborum.
+                                       </p>
+                                    </div>
+                                 </div>
+                              </div>
+                              <hr />
+                              <div className="w-full flex justify-start items-center h-[13%] bg-slate-50 px-2">
+                                 <div>
+                                    <PostIcons />
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
                      </Fragment>
                   )}
                </div>

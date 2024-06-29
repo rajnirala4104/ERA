9e236c84c99d39b9fac51ec534e37c7958daf58f
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-06-29 21:48:37 +0530

    docs(followApiServices.ts): add comments

--- FRONTED/ERA/src/api/services/followApiServices.ts
+++ FRONTED/ERA/src/api/services/followApiServices.ts
@@ -24,38 +24,77 @@ export const getAllTheFollowersOfAPerticularUserApiCall = (userId: string, userT
     return http.get(url, config);
 }
 
+/**
+ * This function makes an HTTP GET request to the backend API to fetch all the followings
+ * of a particular user. It takes in the user's ID and authentication token as parameters.
+ *
+ * @param {string} userId - The ID of the user whose followings we want to fetch.
+ * @param {string} userToken - The authentication token of the user making the request.
+ * @returns {Promise} A Promise that resolves to the HTTP response from the server.
+ */
 export const getAllTheFollowingsOfAPerticularUserApiCall = (userId: string, userToken: string) => {
 
+    // Construct the URL for the API request by appending the user's ID to the endpoint URL.
+    const url = `${FOLLOW_ENDPOINT.getAllTheFollowingsOfAPerticularUser}/${userId}`;
+
+    // Set up the request configuration with the appropriate headers.
     const config = {
         "Content-type": "application/json",
         headers: {
-            Authorization: `Bearer ${userToken}`,
+            Authorization: `Bearer ${userToken}`, // Attach the user's authentication token to the request.
         },
     };
 
-    return http.get(`${FOLLOW_ENDPOINT.getAllTheFollowingsOfAPerticularUser}/${userId}`, config)
+    // Make the HTTP GET request to the API endpoint with the constructed URL and request configuration.
+    return http.get(url, config);
 }
 
+/**
+ * This function makes an HTTP DELETE request to the backend API to unfollow a particular user.
+ * It takes in the ID of the user being unfollowed and the authentication token of the user making the request.
+ *
+ * @param {string} followedUserId - The ID of the user being unfollowed.
+ * @param {string} userToken - The authentication token of the user making the request.
+ * @returns {Promise} A Promise that resolves to the HTTP response from the server.
+ */
 export const unFollowApiCall = (followedUserId: string, userToken: string) => {
 
+    // Set up the request configuration with the appropriate headers.
     const config = {
-        "Content-type": "application/json",
+        "Content-type": "application/json", // Specify that the request body is in JSON format.
         headers: {
-            Authorization: `Bearer ${userToken}`,
+            Authorization: `Bearer ${userToken}`, // Attach the user's authentication token to the request.
         },
     };
 
-    return http.delete(`${FOLLOW_ENDPOINT.delete}/${followedUserId}`, config)
+    // Construct the URL for the API endpoint by appending the user's ID to the endpoint URL.
+    const url = `${FOLLOW_ENDPOINT.delete}/${followedUserId}`;
+
+    // Make the HTTP DELETE request to the API endpoint with the constructed URL and request configuration.
+    return http.delete(url, config);
 }
 
+/**
+ * This function makes an HTTP POST request to the backend API to follow a particular user.
+ * It takes in the ID of the user being followed and the authentication token of the user making the request.
+ *
+ * @param {string} followedUserId - The ID of the user being followed.
+ * @param {string} userToken - The authentication token of the user making the request.
+ * @returns {Promise} A Promise that resolves to the HTTP response from the server.
+ */
 export const followApiCall = (followedUserId: string, userToken: string) => {
 
+    // Set up the request configuration with the appropriate headers.
     const config = {
+        // Specify that the request body is in JSON format.
         "Content-type": "application/json",
         headers: {
+            // Attach the user's authentication token to the request.
             Authorization: `Bearer ${userToken}`,
         },
     };
 
+    // Make the HTTP POST request to the API endpoint with the constructed URL and request configuration.
+    // The request body contains the ID of the user being followed.
     return http.post(FOLLOW_ENDPOINT.create, { followedUserId }, config);
 }
